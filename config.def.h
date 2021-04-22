7d7e0e6d533c46498f2c731c223e30b93c5f82d2
Federico Izzo <federico.izzo99@gmail.com>
2021-04-22 16:48:29 +0200

    add copyoutput script

--- config.def.h
+++ config.def.h
@@ -201,6 +201,8 @@ static MouseShortcut mshortcuts[] = {
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
 
+static char *copyoutput[] = { "/bin/sh", "-c", "st-copyout", "externalpipe", NULL };
+
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
 	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
@@ -219,6 +221,7 @@ static Shortcut shortcuts[] = {
     { MODKEY,               XK_t,           newterm,        {.i =  0} },
     { MODKEY,               XK_k,           kscrollup,      {.i = -1} },
 	{ MODKEY,               XK_j,           kscrolldown,    {.i = -1} },
+    { MODKEY,               XK_o,           externalpipe,   {.v = copyoutput } },
 };
 
 /*
